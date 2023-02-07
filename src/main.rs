6a76c762b5426837aa4269656c447da1381d34f0
Dave Hay <davehay93@gmail.com>
2023-02-07 12:55:24 -0800

    get_word fn

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use rand::Rng;
-use std::io::{self, BufRead};
+use rand::{thread_rng, Rng};
+// use std::io::{self, BufRead};
 
 const WORDS: [&str; 3] = ["hi", "hello", "nuts"];
 
+fn get_word() -> String {
+    let mut rng = thread_rng();
+    let n = rng.gen_range(0..WORDS.len());
+    WORDS[n].to_string()
+}
+
 fn main() {
-    let mut rng = rand::thread_rng();
-    for word in WORDS {
-        println!("{}", word)
-    }
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        println!("Hello, {}!", line.unwrap());
-    }
+    let w = get_word();
+    println!("word is {}", w);
 }
